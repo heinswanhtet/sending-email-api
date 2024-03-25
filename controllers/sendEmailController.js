3170d732739499813d217e6ec688492b1c50e753
Hein Swan Htet <hshscientist@gmail.com>
2024-03-25 14:08:58 +0630

    Added mailing functionality with mailgun

--- controllers/sendEmailController.js
+++ controllers/sendEmailController.js
@@ -1,7 +1,11 @@
 const nodemailer = require('nodemailer')
 const { StatusCodes } = require('http-status-codes')
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
 
-const sendEmail = async (req, res) => {
+const sendEmailEthereal = async (req, res) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -20,6 +24,20 @@ const sendEmail = async (req, res) => {
     })
 
     res.status(StatusCodes.OK).json({ info })
+}
+
+const sendEmail = async (req, res) => {
+    const DOMAIN = process.env.MAILGUN_DOMAIN
+
+    const info = await mg.messages.create(DOMAIN, {
+        from: `Excited User <testsender@mail.com>`,
+        to: ["verifiedMailInMailgunSanbox@mail.com"],
+        subject: "Hello",
+        text: "Testing some Mailgun awesomeness!",
+        html: "<h1>Testing some Mailgun awesomeness!</h1>"
+    })
+
+    res.status(StatusCodes.OK).json(info)
 }
 
 module.exports = sendEmail
