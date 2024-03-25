97988f2a5896f0564d0d1b80f0b18227cf1284ec
Hein Swan Htet <hshscientist@gmail.com>
2024-03-25 12:25:19 +0630

    Added mailing functionality with ethereal

--- controllers/sendEmailController.js
+++ controllers/sendEmailController.js
@@ -1,5 +1,25 @@
+const nodemailer = require('nodemailer')
+const { StatusCodes } = require('http-status-codes')
+
 const sendEmail = async (req, res) => {
-    res.send('sending email')
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.ETHEREAL_USER,
+            pass: process.env.ETHEREAL_PASS
+        }
+    })
+
+    const info = await transporter.sendMail({
+        from: '"Luffy 👻" <luffy@gmail.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    })
+
+    res.status(StatusCodes.OK).json({ info })
 }
 
 module.exports = sendEmail
